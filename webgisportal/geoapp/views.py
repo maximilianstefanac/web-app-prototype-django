f1e8d44ae94d8645d027be761dbabb4824b9391e
Maximilian Stefanac <maximilian.stefanac@googlemail.com>
2019-12-28 03:08:23 +0100

    using geodjango serializer to create geojson data from Tree entity

--- webgisportal/geoapp/views.py
+++ webgisportal/geoapp/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.serializers import serialize
 from geoapp.models import Tree
 
 # Create your views here.
 def index(request):
     trees = Tree.objects.all()
 
-    response = ''
+    # response = ''
 
-    for tree in trees:
+    # for tree in trees:
+    #     response = response + "id: {0}, position: {1}".format(str(tree.tree_id), str(tree.position.coords))
 
-        tree_string = "id: %s, position: %s".format(str(tree.tree_id), str(tree.position))
-        response.join(tree_string)
+    response = serialize('geojson', trees, geometry_field='position', fields=('tree_id',))
 
     return HttpResponse(response)
